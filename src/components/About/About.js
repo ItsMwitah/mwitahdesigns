d73088d5f606572324467e068c88d5dbfe1e1690
ItsMwitah <136892656+ItsMwitah@users.noreply.github.com>
2024-11-29 16:45:00 +0300

    Update About Section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,11 +20,11 @@ const About = () => {
         <AboutText>
           Hi there! I'm Job Mwita, a Freelance Email Designer based in Nairobi, Kenya. My goal is to create stunning email designs that engage and convert - whether you’re building your brand, boosting sales, or nurturing leads, I’m here to help you captivate your audience and drive results.                                                  
           <br />
+          <br />
           It's not just about making things look pretty – it's about driving engagement, building relationships, and ultimately, boosting your bottom line.
           <br />
+          <br />
           I’m all about making design a fun and collaborative process, and as stress-free for you as possible. To do this, I always follow a specific process so you know exactly what to expect when working with me. 
-          
-          ✅ Ready to enhance your email marketing? Book My FREE 1:1 Power Hour or DM me on my LinkedIn Profile below!
         </AboutText>
       </AboutDetails>
     </StyledAbout>
