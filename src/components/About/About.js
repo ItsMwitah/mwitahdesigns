c29f98ff3072ecd3463035f3a495e3641e432065
ItsMwitah <136892656+ItsMwitah@users.noreply.github.com>
2024-11-29 16:51:29 +0300

    Add Stars

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,13 +18,13 @@ const About = () => {
       </SectionTitle>
       <AboutDetails>
         <AboutText>
-          Hi there! I'm Job Mwita, a Freelance Email Designer based in Nairobi, Kenya. My goal is to create stunning email designs that engage and convert - whether you’re building your brand, boosting sales, or nurturing leads, I’m here to help you captivate your audience and drive results.                                                  
+        ⭐Hi there! I'm Job Mwita, a Freelance Email Designer based in Nairobi, Kenya. My goal is to create stunning email designs that engage and convert - whether you’re building your brand, boosting sales, or nurturing leads, I’m here to help you captivate your audience and drive results.                                                  
           <br />
           <br />
-          It's not just about making things look pretty – it's about driving engagement, building relationships, and ultimately, boosting your bottom line.
+        ⭐It's not just about making things look pretty – it's about driving engagement, building relationships, and ultimately, boosting your bottom line.
           <br />
           <br />
-          I’m all about making design a fun and collaborative process, and as stress-free for you as possible. To do this, I always follow a specific process so you know exactly what to expect when working with me. 
+        ⭐I’m all about making design a fun and collaborative process, and as stress-free for you as possible. To do this, I always follow a specific process so you know exactly what to expect when working with me.
         </AboutText>
       </AboutDetails>
     </StyledAbout>
