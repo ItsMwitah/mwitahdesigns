624b2a7b2b61656568b2d4d8f6c77d97fe786414
ItsMwitah <136892656+ItsMwitah@users.noreply.github.com>
2024-11-29 16:42:07 +0300

    Update About Section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,20 +14,16 @@ const About = () => {
     <StyledAbout id="about">
       <SectionSupTitle>ABOUT ME</SectionSupTitle>
       <SectionTitle>
-        HELPING EMAIL MARKETING AGENCIES <br /> DELIVER CONVERSION-DRIVEN DESIGNS
+        HELPING EMAIL MARKETING AGENCIES & ECOM<br /> BRANDS DELIVER CONVERSION-DRIVEN DESIGNS
       </SectionTitle>
       <AboutDetails>
         <AboutText>
-          Hi there! I'm Job Mwita, a Freelance Email Designer based in Nairobi, Kenya. 𝗠𝘆 𝗴𝗼𝗮𝗹 𝗶𝘀 𝘁𝗼 𝗰𝗿𝗲𝗮𝘁𝗲 𝘀𝘁𝘂𝗻𝗻𝗶𝗻𝗴 𝗲𝗺𝗮𝗶𝗹 𝗱𝗲𝘀𝗶𝗴𝗻𝘀 𝘁𝗵𝗮𝘁 𝗲𝗻𝗴𝗮𝗴𝗲 𝗮𝗻𝗱 𝗰𝗼𝗻𝘃𝗲𝗿𝘁—whether you’re building your brand, boosting sales, or nurturing leads, I’m here to help you captivate your audience and drive results.                                                  
+          Hi there! I'm Job Mwita, a Freelance Email Designer based in Nairobi, Kenya. My goal is to create stunning email designs that engage and convert - whether you’re building your brand, boosting sales, or nurturing leads, I’m here to help you captivate your audience and drive results.                                                  
           <br />
+          It's not just about making things look pretty – it's about driving engagement, building relationships, and ultimately, boosting your bottom line.
           <br />
-          🎨 HERE IS HOW I CAN HELP YOU:<br />
-          • Create Templates in Figma / Canva / Adobe Photoshop that you can Reuse<br />
-          • Convert your JPEG / Canva / Photoshop / Figma design into Klaviyo / Mailchimp Emails<br />
-          • Create Tutorial Videos 𝗙𝗼𝗿 𝗬𝗼𝘂 or 𝗬𝗼𝘂𝗿 𝗧𝗲𝗮𝗺<br />
-          • Custom NO CODE Klaviyo / Mailchimp Templates that are editable and reusable!<br />
-          • Teach you how to do any of this 𝗬𝗢𝗨𝗥𝗦𝗘𝗟𝗙:)<br />
-          <br />
+          I’m all about making design a fun and collaborative process, and as stress-free for you as possible. To do this, I always follow a specific process so you know exactly what to expect when working with me. 
+          
           ✅ Ready to enhance your email marketing? Book My FREE 1:1 Power Hour or DM me on my LinkedIn Profile below!
         </AboutText>
       </AboutDetails>
